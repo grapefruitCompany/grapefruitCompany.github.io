1e212581d53bc741affe350c678df5462c9b7462
Sergey Rumiantsev <grapefruitcompany@gmail.com>
2019-03-26 07:06:05 +0200

    react props

--- test-react/src/App.js
+++ test-react/src/App.js
@@ -5,17 +5,23 @@ import { Home } from "./components/Home";
 
 class App extends Component {
   render() {
+    let user = {
+      name: 'Anna',
+      hobbies: ['sport','yoge','movies',] 
+    }
     return (
       <div className="container">
         <div className="row">
-            <div className="col-xs-10 col-xs-offset-1">
-                <Header/>
-            </div>
+          <div className="col-xs-10 col-xs-offset-1">
+            <Header/>
+          </div>
         </div>
         <div className="row">
-            <div className="col-xs-10 col-xs-offset-1">
-                <Home/>
-            </div>
+          <div className="col-xs-10 col-xs-offset-1">
+            <Home name={'Max'} age={27} user={user}>
+              <p>This is a paragraph!</p>
+            </Home>
+          </div>
         </div>
     </div>
     );
